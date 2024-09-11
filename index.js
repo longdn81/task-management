ab8f20ff8d8d54869bf6e79d9e8c0e25c1acef37
longdn81 <146925221+longdn81@users.noreply.github.com>
2024-09-11 23:27:34 +0700

    thay đổi trạng thái công việc

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 require(`dotenv`).config();
 
+const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+
 const database = require("./config/database");
 
 const routesApiVer1 = require("./api/v1/routes/index.route");
 
 const app = express();
 const port = process.env.PORT;
+
+// parse application/json
+app.use(bodyParser.json())
 
 database.connect();
 
