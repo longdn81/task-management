41b2956f63318c4077086cd5ace58a3a5f6d9d20
longdn81 <146925221+longdn81@users.noreply.github.com>
2024-09-11 22:24:34 +0700

    sort tasks

---
+++ api/v1/controllers/task.controller.js
@@ -0,0 +1,34 @@
+const Task = require("../../../models/task.model");
+// [GET] /tasks
+module.exports.index =  async (req, res) => {
+    const find = {
+        deleted : false ,
+    };
+
+    if(req.query.status){
+        find.status = req.query.status ;
+    }
+    // Sort
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue ){
+        sort[req.query.sortKey] = req.query.sortValue ;
+    }
+    // End Sort
+    const tasks = await Task.find(find).sort(sort);
+
+    res.json(tasks);
+};
+// [GET] /tasks/detail/:id
+module.exports.detail  = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.json(task);
+    } catch (error) {
+        res.json("Không tìm thấy!");
+    }
+};
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -1,30 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-const Task = require("../../../models/task.model");
+const controller = require("../controllers/task.controller")
 
+router.get('/', controller.index);
 
-router.get("/", async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false,
-    })
+router.get('/detail/:id', controller.detail);
 
-    console.log(tasks);
-
-    res.json(tasks);
-});
-
-router.get("/detail/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-        res.json(task);
-    } catch (error) {
-        res.json("Không tìm thấy!");
-    }
-});
 
 module.exports = router;
